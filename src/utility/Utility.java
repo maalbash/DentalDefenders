6a613bccb40eabe7637d948684340f12b3d6e375
Mohamed Albashir <mohz2009@hotmail.co.uk>
2017-04-08 05:50:36 -0400

    added a helper function

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -1,6 +1,8 @@
 package utility;
 
+import objects.Player;
 import processing.core.PConstants;
+import processing.core.PVector;
 
 /**
  * Created by ujansengupta on 3/30/17.
@@ -17,5 +19,9 @@ public class Utility
             else
                 return (r + 2 * PConstants.PI);
         }
+    }
+
+    public static boolean checkTargetReached(Player player, PVector target) {
+        return((Math.abs(target.x - player.getPosition().x) <= 1.f) && (Math.abs(target.y - player.getPosition().y) <= 1.f));
     }
 }
